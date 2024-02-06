95c977c50d8a98b0bacfb67eec34e637c654e2ae
danie <danie@Daniel.home>
2024-02-06 19:19:21 +0000

    This is the assessment

--- src/assessment12/SizeFinder.java
+++ src/assessment12/SizeFinder.java
@@ -1,17 +1,25 @@
 package assessment12;
 
 /**
+ * Module: Software Development: Programming Foundations
+ * Student: Daniel Barbu: 20024094
+ * Application: Assessment1_2
+ * Version: 1.0
+ * Date: 6/02/2024
+ * Files: Main.java, SizeFinder.java, FindAverage.java
  *
- * @author danie
+ * @author Daniel Barbu
  */
 
 public class SizeFinder {
     // Method to find the largest element in the array
     public static int findLargest(int[] storageSpaces) {
-        int largest = storageSpaces[0]; // Assume the first element is the largest initially
+        // Assume the first element is the largest initially
+        int largest = storageSpaces[0]; 
         for (int size : storageSpaces) {
             if (size > largest) {
-                largest = size; // Update largest if current size is greater
+                // Update largest if current size is greater
+                largest = size; 
             }
         }
         return largest;
